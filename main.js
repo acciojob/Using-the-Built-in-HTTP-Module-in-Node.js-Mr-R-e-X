a708dd8e0b536da44bfd3506bb5b352a2b4c962a
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-10 10:31:01 +0530

    done

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const http = require("http");
-
+const fs = require("fs");
 let server = http.createServer((req, res) => {
-  res.end("Hello, world!");
+  fs.readFile("output.txt", "utf8", (err, data) => {
+    if (err) {
+      console.log("Hello, world!");
+      res.end("Hello, world!");
+    } else {
+      if (data.toString() === "") {
+        console.log("Hello, world!");
+        return res.end("Hello, world!");
+      }
+      console.log(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, () => {
